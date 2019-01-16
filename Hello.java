9171ee3be0b3f2442bcac988cb6ffbae0b0df1ee
smithnp2 <46756647+smithnp2@users.noreply.github.com>
2019-01-16 11:59:39 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+/* Hello.java
+#################################################
+# Java test 
+#################################################
+# Nathaniel Smith
+# 1/16/19
+# CMSC 255 - 004
+#################################################
+*/
+
+
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name");
+		System.out.println("Java Project Name");
+		System.out.println("Project DDescription");
+		System.out.println("Version Date");
+		System.out.println("Course Number and Section");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
