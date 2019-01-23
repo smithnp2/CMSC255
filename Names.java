1e105168e1926757e8862b4ff77e3f86dc7a72e7
smithnp2 <46756647+smithnp2@users.noreply.github.com>
2019-01-23 11:56:17 -0500

    Add files via upload

---
+++ Names.java
@@ -0,0 +1,22 @@
+
+
+
+ public class Names{
+	 
+	 public static void main(String[] args){
+		 System.out.println("-----------------------------");
+		 System.out.println("This program was written by Charles Legard and Nathaniel Smith.");
+		 System.out.println("-----------------------------");
+		 
+		 
+		 
+		 
+		 
+		 
+	 }//end MAIN
+	 
+	 
+	 
+	 
+	 
+ }//end of program
---
+++ SquareTriangle.java
@@ -0,0 +1,22 @@
+/* SquareTriangle.java
+*********************
+* Lab 2
+*********************
+* Nathaniel and Charles - Group 3
+* 1/23/19
+* CMSC 255 - 004
+*********************
+*/
+
+public class SquareTriangle{
+	
+	public static void main(String[]args){
+		System.out.println("#############");
+		System.out.println("#     #     #");
+		System.out.println("#    # #    #");
+		System.out.println("#   #   #   #");
+		System.out.println("#  #     #  #");
+		System.out.println("# #       # #");
+		System.out.println("#############");
+		}
+	}
